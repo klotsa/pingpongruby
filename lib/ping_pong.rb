f5b952fb63954aa0a22d3aed9075b4ccd13b8439
anatoliy klots and zach beecher <zach@>
2017-04-17 11:15:16 -0700

    test three conditions

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,11 +1,16 @@
 class Fixnum
   define_method(:check_ping?) do
-    numbers = (1..self)
-    pings = Array.new
+    numbers = (1..20)
+   pings = Array.new
     numbers.each() do |number|
       if number.%(15).eql?(0)
-        pings.push("Ping-Pong")
-      end
-
-
+       pings.push("Ping-Pong")
+     elsif number.%(5).eql?(0)
+       pings.push("Pong")
+     elsif number.%(3).eql?(0)
+     pings.push("Ping")
+     end
+    end
+    pings
   end
+end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -3,6 +3,12 @@ require('ping_pong')
 
 describe('Fixnum#ping_pong') do
   it("it will be div by 15") do
-    expect((15).check_ping?()).to(eq)(0))
+    expect((15).check_ping?()).to(eq(["Ping", "Pong", "Ping", "Ping", "Pong", "Ping", "Ping-Pong","Ping", "Pong"]))
+  end
+  it("it will be div by 5") do
+    expect((5).check_ping?()).to(eq(["Ping", "Pong", "Ping", "Ping", "Pong", "Ping", "Ping-Pong","Ping", "Pong"]))
+  end
+  it("it will be div by 3") do
+    expect((3).check_ping?()).to(eq(["Ping", "Pong", "Ping", "Ping", "Pong", "Ping", "Ping-Pong","Ping", "Pong"]))
   end
 end
